f989472dc7b383dd18f3e16e6b4642321d088a91
riky <richardareid2002@googlemail.com>
2015-09-09 10:42:20 +0000

    Exercise-03-08

---
+++ UnravelNG/Exercise-03/modules/maintenance.js
@@ -0,0 +1,8 @@
+angular.module('maintenance', [])
+    .controller('locationsCtrl', LocationsCtrl)
+    .controller('sitesCtrl', SitesCtrl)
+    .controller('typesCtrl', TypesCtrl);
+
+function LocationsCtrl(currentSpot) {}
+function SitesCtrl(currentSpot) {}
+function TypesCtrl(currentSpot) {}
